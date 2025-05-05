65f54b1d1639e83ab5e3a59080203eae03d4bd15
jpwiig <jpwiig@gmail.com>
2025-05-05 10:27:06 +0200

    checks if it is windy or not

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("ready!")
     let kommune
-    document.getElementById("btn-submit").addEventListener("click", event => {
+    document.getElementById("btn-submit").addEventListener('click', event => {
+        event.preventDefault()
         kommune = document.getElementById("kommune").value
         console.log(kommune)
 
@@ -11,12 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function findKommune(kommune) {
     let url = "https://api.kartverket.no/kommuneinfo/v1/sok"
-    const data = await fetch(url + `?navn=${kommune}`).then(response => response.json()).then(data => {
+    const data = await fetch(url + `?knavn=${kommune}`,{ method : 'GET'}, 
+
+    ).then(response => response.json()).then(data => {
         return data.kommuner[0];
     })
     const kommunenr = data.kommunenummer
-    document.getElementById("result").innerHTML = "<p> blåser det i " + data.kommunenavn + "?</p>"
-   getwind(data.punktIOmrade.coordinates[0], data.punktIOmrade.coordinates[1])
+   
+ 
+   const wind = await getwind(data.punktIOmrade.coordinates[0], data.punktIOmrade.coordinates[1])
+   console.log(wind)
+    let ut = `<p> blåser det i  ${data.kommunenavn}  ?</p><p>m/s: ${wind}</p><p>${windy(wind)}</p>`
+   document.getElementById("result").innerHTML = ut
 }
 
 //todo add a header here
@@ -28,4 +35,11 @@ async function getwind(lat, long) {
     ).then(data => {
         return data.properties.timeseries[0].data.instant.details.wind_speed
     })
+    return data
+}
+function windy(wind){
+    if(wind == 0.00 ) return "det blæs ittj"
+    if (wind >= 1.00 && wind < 6.00) return "det blæs litt"
+    if (wind  <= 6.00 && wind > 10.00) return "det blæs"
+    if (wind >= 10.00) return "det blæs veldig mye"
 }
